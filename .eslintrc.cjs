d6f163b7df16447229ca7aeaf32e5d4b57e62c5d
Aleksandr <zheltkevich42@gmail.com>
2021-12-01 18:46:48 +0300

    Test

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/base',
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:vue/vue3-recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <span />
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+.app {
+  background-color: #000fff;
+}
+</style>
